7167bdb5564e188122d9fd2d70d6d9ea366fc57b
Leonardo Costa <leosilva.sjc@gmail.com>
2022-01-28 01:09:43 -0300

    intitial commit

---
+++ src/test/java/one/digitalInnovation/PersonApi/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalInnovation.PersonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
